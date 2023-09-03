976d5b4105dafa9b0274816904d2e08a28db650a
Danylo Hukov <48653308+DHukov@users.noreply.github.com>
2023-09-03 20:12:08 +0200

    Character Movement

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 movement;
+    [SerializeField] private CharacterController characterController;
+    
+    [SerializeField] private float movementSpeed = 10;
+    [SerializeField] private float rotationSpeed = 0.1f;
+    private float turnSmoothVelocity;
+    void Update()
+    {
+        Movement();
+    }
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Movement()
+    {
+        float horizontal_X = Input.GetAxisRaw("Horizontal");
+        float vertical_Z = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(horizontal_X, 0, vertical_Z).normalized;
+
+        if(movement.magnitude >= 0.1f)
+        {
+            float characterAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, characterAngle, ref turnSmoothVelocity, rotationSpeed);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            characterController.Move(movement * movementSpeed * Time.deltaTime); 
+        }
+    }
+   
+}
